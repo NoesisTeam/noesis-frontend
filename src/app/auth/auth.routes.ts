f81db2fcf552a834a7c77b0279c271cd1f8058c0
danieltorrez29 <daniel.torres04@uptc.edu.co>
2024-10-22 13:16:41 -0500

    build(auth module): signup component creation
    adding essential html form label attributes (login & signup)

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const AUTH_ROUTES: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
