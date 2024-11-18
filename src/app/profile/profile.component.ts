dd82f9b3aee3f171298b520bdb74f7fb2ed187e1
JorgeEdilMz <jorge.martinez09@uptc.edu.co>
2024-11-17 19:37:41 -0500

    update profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForOf, NgIf, NgStyle } from "@angular/common";
+import { NgForOf, NgIf, NgStyle } from '@angular/common';
 
 interface Medal {
   text: string;
@@ -10,12 +10,8 @@ interface Medal {
   selector: 'app-progress',
   standalone: true,
   templateUrl: './profile.component.html',
-  imports: [
-    NgStyle,
-    NgForOf,
-    NgIf
-  ],
-  styleUrls: ['./profile.component.css']
+  imports: [NgStyle, NgForOf, NgIf],
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
   genericMedalIcon: string = '/assets/icons/generic-medal.png';
@@ -26,12 +22,13 @@ export class ProfileComponent {
     { text: 'Reached 40 pts', color: '#ffd700' },
     { text: 'Completed 5 quizzes', color: '#4b0082' },
     { text: 'Special Achievement', color: '#2a9d8f' },
-    // Añadir más medallas para pruebas
   ];
 
   visibleMedals: Medal[] = [];
   currentIndex = 0;
   maxVisibleMedals = 4;
+
+  isCircleVisible: boolean = true; // Controla la visibilidad del círculo
 
   get showLeftButton(): boolean {
     return this.currentIndex > 0;
@@ -46,12 +43,13 @@ export class ProfileComponent {
   }
 
   updateVisibleMedals() {
-    // Muestra una medalla adicional al desplazar el carrusel
-    this.visibleMedals = this.medals.slice(this.currentIndex, this.currentIndex + this.maxVisibleMedals);
+    this.visibleMedals = this.medals.slice(
+      this.currentIndex,
+      this.currentIndex + this.maxVisibleMedals
+    );
   }
 
   scrollLeft() {
-    // Desplaza hacia la izquierda solo una posición si no estamos en el primer índice
     if (this.currentIndex > 0) {
       this.currentIndex--;
       this.updateVisibleMedals();
@@ -59,7 +57,6 @@ export class ProfileComponent {
   }
 
   scrollRight() {
-    // Desplaza hacia la derecha solo una posición si no hemos alcanzado el final
     if (this.currentIndex + this.maxVisibleMedals < this.medals.length) {
       this.currentIndex++;
       this.updateVisibleMedals();
